c09f3b177a5f4b842bc1af3eae61f3abfa4cc4fd
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-30 21:10:53 +0100

    init

---
+++ SwagProject/SwagProject/Pages/ProductPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using SwagProject.Driver;
+
+namespace SwagProject.Pages
+{
+    public class ProductPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+
+        public IWebElement AddBackPac => driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+        public IWebElement AddT_Shirt => driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt"));
+        public IWebElement Cart => driver.FindElement(By.CssSelector("#shopping_cart_container .shopping_cart_badge"));
+
+    }
+    
+}
---
+++ SwagProject/SwagProject/Test/BuyProductTest.cs
@@ -0,0 +1,34 @@
+using SwagProject.Driver;
+using SwagProject.Pages;
+
+namespace SwagProject.Test
+{
+    public class Tests
+    {
+        LoginPage loginPage;
+        ProductPage productPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            productPage = new ProductPage();
+        }
+
+        [TearDown]
+        public void ClosePage()
+        {
+            WebDrivers.CleanUp();
+        }
+        [Test]
+        public void TC01_AddTwoNewProductInCart_ShouldDisplayedTwoProducts()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.AddBackPac.Click();
+            productPage.AddT_Shirt.Click();
+
+            Assert.That("2",Is.EqualTo(productPage.Cart.Text));
+        }
+    }
+}
